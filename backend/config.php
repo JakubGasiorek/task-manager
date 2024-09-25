66d12a1bff93b0b5fc431f6f5685b6fa6f0eb168
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-09-25 19:01:29 +0200

    Deleted echo from config php

--- backend/config.php
+++ backend/config.php
@@ -7,7 +7,6 @@ $password = getenv('MYSQL_PASSWORD');
 try {
     $connect = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
